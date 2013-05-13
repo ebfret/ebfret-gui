41d5e26aa7c73826cd7b6b69041875b5c07576fa
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-05-13 11:11:19 -0400

    ebfret.plot.scale: renormalizes a set of line plots

---
+++ src/+ebfret/+plot/scale.m
@@ -0,0 +1,10 @@
+function lines = scale(lines, s)
+    % lines = scale(lines, s)
+    % 
+    % Rescales a set of lines by multiplication with s. 
+    if isscalar(s)
+        s = repmat(s, size(lines))
+    end
+    for l = 1:length(lines(:))
+        lines(l).ydata = s(l) * lines(l).ydata;
+    end
