917e277d3e96719492df731336eea8319c0c6cdb
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-05-14 16:56:50 -0400

    ebfret.analysis.hmm.h_step: optionally iterate over updates
    If expectation values are supplied the posterior is iteratively
    recalculated from the updated prior. This allows faster convergence.

--- src/+ebfret/+analysis/+hmm/h_step.m
+++ src/+ebfret/+analysis/+hmm/h_step.m
@@ -1,5 +1,5 @@
-function u = hstep(w)
-% u = hstep(w)
+function [u, kl] = hstep(w, varargin)
+% u = hstep(w, varargin)
 %
 % Hyper parameter updates for empirical Bayes inference (EB)
 % on a single-molecule FRET dataset.
@@ -51,17 +51,97 @@ function u = hstep(w)
 %       Hyperparameters for the prior distribution p(theta | u)
 %       (same fields as w)
 
-% run normal-gamma updates for emission model parameters
-[u_mu, u_beta, u_a, u_b] = ...
-    ebfret.analysis.dist.normgamma.h_step(...
-        cat(2, w.mu), cat(2, w.beta), 0.5 * cat(2, w.nu), 0.5 ./ cat(2, w.W));
-u.mu = u_mu;
-u.beta = u_beta;
-u.nu = 2 * u_a;
-u.W = 0.5 ./ u_b;
-% add dirichlet updates for transition matrix
-u.A = ebfret.analysis.dist.dirichlet.h_step({w.A});
-% add dirichlet updates for initial state probabilities
-u.pi = ebfret.analysis.dist.dirichlet.h_step({w.pi});
-% ensure field names in correct order
+ip = inputParser();
+ip.StructExpand = true;
+ip.addOptional('u', struct([]), @isstruct);       
+ip.addParamValue('expect', struct([]), @isstruct);       
+ip.addParamValue('max_iter', 100, @isscalar);       
+ip.addParamValue('threshold', 1e-5, @isscalar);       
+ip.parse(varargin{:});
+args = ip.Results;
+
+args.max_iter = 100;
+args.threshold = 1e-4;
+
+% cast posterior in array form
+w_mu = cat(2, w.mu);
+w_beta = cat(2, w.beta);
+w_a = 0.5 * cat(2, w.nu);
+w_b = 0.5 ./ cat(2, w.W);
+w_A = cat(3, w.A);
+w_pi = cat(2, w.pi);
+
+u_old = args.u;
+u = struct();
+it = 0;
+kl = [];
+while true 
+    % run normal-gamma updates for emission model parameters
+    [u.mu, u.beta, u.a, u.b] = ...
+        ebfret.analysis.dist.normgamma.h_step(...
+            w_mu, w_beta, w_a, w_b, 1);
+    u.nu = 2 * u.a;
+    u.W = 0.5 ./ u.b;
+    % add dirichlet updates for transition matrix
+    u.A = ebfret.analysis.dist.dirichlet.h_step(w_A);
+    % add dirichlet updates for initial state probabilities
+    u.pi = ebfret.analysis.dist.dirichlet.h_step(w_pi);
+
+    % check if we need to break
+    if (it >= args.max_iter) 
+        break
+    end
+    if (it >= 1)
+        kl(it) = ebfret.analysis.hmm.kl_div(u, u_old);
+    end
+    if (it >= 2) & (kl(it-1) - kl(it)) ./ (1 - kl(it)) < args.threshold
+        break
+    end 
+
+    % get sufficient statistics
+    if (it == 0) && ~isempty(args.expect)
+        E_zz = cat(3, args.expect.zz);
+        E_z1 = arrayfun(@(e) e.z(1,:)', args.expect, 'UniformOutput', false);
+        E_z1 = cat(2, E_z1{:});
+        E_z = arrayfun(@(e) sum(e.z,1)', args.expect, 'UniformOutput', false);
+        E_z = cat(2, E_z{:});
+        E_x = cat(2, args.expect.x);
+        V_x = cat(2, args.expect.xx) - E_x.^2;
+        % % E_zz(k,l) = w.A(k,l) - u.A(k,l)
+        % E_zz = bsxfun(@minus, w_A, u_old.A);
+        % E_z1 = bsxfun(@minus, w_pi, u_old.pi);
+        % % E_z(k) = w.beta(k) - u.beta(k)
+        % E_z = bsxfun(@minus, w_beta, u_old.beta);
+        % % E_x(k) = (w.beta(k) w.mu(k) - u.beta(k) u.mu(k)) / E_z(k)
+        % E_x = bsxfun(@minus, ... 
+        %         w_beta .* w_mu, ...
+        %         u_old.beta .* u_old.mu) ./ E_z;
+        % % V_x(k) = ((1/w.W(k) - 1/u.W(k)) 
+        % %            - u.beta(k) E_z(k) / (w.beta(k)) (E_z(k) - u.mu(k)).^2) 
+        % %           ./ E_z
+        % V_x = 2 * (bsxfun(@minus, w_b, 0.5 ./ u_old.W) ...
+        %            - (bsxfun(@times, u_old.beta, E_z) ./ w_beta ...
+        %               .* bsxfun(@minus, E_x, u_old.mu).^2)) ./ E_z;
+    elseif isempty(args.expect)
+        break
+    end
+
+    % update posteriors
+    w_pi = bsxfun(@plus, E_z1, u.pi); 
+    w_A = bsxfun(@plus, E_zz, u.A);
+    w_beta = bsxfun(@plus, E_z, u.beta);
+    w_mu = bsxfun(@plus, E_z .* E_x, u.beta .* u.mu) ./ w_beta;
+    w_a = bsxfun(@plus, 0.5 * E_z, u.a);
+    w_b = 0.5 * bsxfun(@plus, ...
+                    2 * u.b, ...
+                    E_z .* V_x ...
+                    + (bsxfun(@times, E_z, u.beta) ./ w_beta ...
+                        .* bsxfun(@minus, E_x, u.mu).^2));
+
+    % increment iteration count
+    it = it+1;
+    u_old = u;
+end
+
+u = rmfield(u, {'a', 'b'});
 u = orderfields(u, fieldnames(w));
