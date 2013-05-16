ff4ff3745f9c673c0db7a88b18d658ed6fc23d18
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-05-16 16:07:41 -0400

    ebfret.ui.MainWindow: modify clipping behaviour
    * No longer clip at [-0.2 1.2] by default
    * Clip dialog now allows specification of exlusion threshold

--- src/+ebfret/+ui/@MainWindow/clip_outliers.m
+++ src/+ebfret/+ui/@MainWindow/clip_outliers.m
@@ -1,13 +1,27 @@
-function clip_outliers(self, x_lim)
+function clip_outliers(self, x_lim, max_outliers)
     if nargin < 2
-        x_lim = ...
-            inputdlg({'Clip at Minimum:', 'Clip at Minimum:'}, ...
-                'Select Range', 1, {'-0.2', '1.2'});
-        x_lim = [str2num(x_lim{1}) str2num(x_lim{2})];
+        dlg = ...
+            inputdlg({'Clip at Minimum:', 'Clip at Maximum:', 'Exclude when number of Outliers exceeds:'}, ...
+                'Select Range', 1, {'-0.2', '1.2', '10'});
+        x_lim = [str2num(dlg{1}) str2num(dlg{2})];
+        max_outliers = str2num(dlg{3});
     end
     for n = 1:length(self.series)
+        % clip outlier points
         x = self.series(n).signal;
         self.series(n).signal(x < x_lim(1)) = x_lim(1);
         self.series(n).signal(x > x_lim(2)) = x_lim(2);
+        % check if too many outliers
+        x = self.series(n).signal(self.series(n).clip.min:self.series(n).clip.max);
+        if (sum(x <= x_lim(1)) + sum(x >= x_lim(2))) > max_outliers
+            self.series(n).exclude = true;
+        else
+            (sum(x <= x_lim(1)) + sum(x >= x_lim(2)))
+        end
     end
+    self.reset_analysis(...
+        self.controls.min_states:self.controls.max_states);
+    self.set_control(...
+        'ensemble', self.controls.ensemble, ...
+        'series', self.controls.series);
     self.refresh('ensemble', 'series');
--- src/+ebfret/+ui/@MainWindow/load_data.m
+++ src/+ebfret/+ui/@MainWindow/load_data.m
@@ -51,8 +51,8 @@ function load_data(self)
             self.series(n).time = (1:length(self.series(n).donor))';
             self.series(n).signal = self.series(n).acceptor ...
                                 ./ (self.series(n).acceptor + self.series(n).donor);
-            self.series(n).signal(self.series(n).signal<-0.2) = -0.2;                                
-            self.series(n).signal(self.series(n).signal>1.2) = 1.2;                                
+            % self.series(n).signal(self.series(n).signal<-0.2) = -0.2;                                
+            % self.series(n).signal(self.series(n).signal>1.2) = 1.2;                                
             self.series(n).exclude = false;
             self.series(n).clip.min = 1;
             self.series(n).clip.max = length(self.series(n).time);
