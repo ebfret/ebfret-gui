17ea44941f39df75125332400a7089e6bff36a39
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-05-22 11:59:36 -0400

    ebfret.plot.scale added missing ;

--- src/+ebfret/+plot/scale.m
+++ src/+ebfret/+plot/scale.m
@@ -3,7 +3,7 @@ function lines = scale(lines, s)
     % 
     % Rescales a set of lines by multiplication with s. 
     if isscalar(s)
-        s = repmat(s, size(lines))
+        s = repmat(s, size(lines));
     end
     for l = 1:length(lines(:))
         lines(l).ydata = s(l) * lines(l).ydata;
