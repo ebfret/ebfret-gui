4ffdd405c7c83dd6bdd30c12b54370b36e34e108
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-05-28 12:43:18 -0400

    ebfret.ui.Mainwindow: Improve photo-bleaching removal
    * Thresholding is now done using a window-averaged signal
    * It is now possible to threshold by sum of donor and acceptor

--- src/+ebfret/+ui/@MainWindow/remove_bleaching.m
+++ src/+ebfret/+ui/@MainWindow/remove_bleaching.m
@@ -17,34 +17,39 @@ function remove_bleaching(self, method, thresholds)
                         % if (ia < (id + tol));
                         %     self.series(n).clip.max = min(ia, id);
                         % else
-                        %     self.series(n).exclude = true;
+                           %     self.series(n).exclude = true;
                         % end
                     end
                 end
             case 2 
                 % remove photobleaching using manual thresholds
+                W = 7;
+                W0 = floor(W/2);
+                kernel = ebfret.analysis.normalize(exp(-linspace(-1.5,1.5,W).^2));
                 for n = 1:length(self.series)
-                    clip_max = self.series(n).clip.max - self.series(n).clip.min;
+                    clip_max = length(self.series(n).signal) - self.series(n).clip.min;
                     if ~isnan(thresholds.fret)
-                        signal = self.series(n).signal(self.series(n).clip.min:end);
+                        signal = conv(self.series(n).signal(self.series(n).clip.min:end), kernel, 'valid');
                         clip_max = ...
                             min(min(clip_max, ...
-                                [find(signal < thresholds.fret, ...
-                                    1, 'first'), inf]));
+                                [find(signal < thresholds.fret, 1, 'first') + W0, inf]));
                     end
-                    if ~isnan(thresholds.acc)
-                        acceptor = self.series(n).acceptor(self.series(n).clip.min:end);
+                    if ~isnan(thresholds.acc) || ~isnan(thresholds.sum)
+                        acceptor = conv(self.series(n).acceptor(self.series(n).clip.min:end), kernel, 'valid');
                         clip_max = ...
                             min(min(clip_max, ...
-                                [find(acceptor < thresholds.acc, ...
-                                    1, 'first'), inf]));
+                                [find(acceptor < thresholds.acc, 1, 'first') + W0, inf]));
                     end
-                    if ~isnan(thresholds.don)
-                        donor = self.series(n).donor(self.series(n).clip.min:end);
+                    if ~isnan(thresholds.don) || ~isnan(thresholds.sum)
+                        donor = conv(self.series(n).donor(self.series(n).clip.min:end), kernel, 'valid');
                         clip_max = ...
                             min(min(clip_max, ...
-                                [find(donor < thresholds.don, ...
-                                    1, 'first'), inf]));
+                                [find(donor < thresholds.don, 1, 'first') + W0, inf]));
+                    end
+                    if ~isnan(thresholds.sum)
+                        clip_max = ...
+                            min(min(clip_max, ...
+                                [find(donor+acceptor < thresholds.sum, 1, 'first') + W0, inf]));
                     end
                     if ~isnan(thresholds.pad)
                         clip_max = ...
@@ -57,6 +62,8 @@ function remove_bleaching(self, method, thresholds)
                         self.series(n).exclude = true;
                     end
                 end
+            otherwise
+                return
         end
         self.reset_analysis(self.controls.min_states:self.controls.max_states);
         self.set_control('clip', struct('max', self.series(self.controls.series.value).clip.max));
