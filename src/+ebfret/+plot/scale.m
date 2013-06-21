119fdd38f8ec58276d0517a304bdcadadfd9e896
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-06-21 10:57:47 -0400

    plot.scale: allow scaling by matrix of weights

--- src/+ebfret/+plot/scale.m
+++ src/+ebfret/+plot/scale.m
@@ -6,5 +6,5 @@ function lines = scale(lines, s)
         s = repmat(s, size(lines));
     end
     for l = 1:length(lines(:))
-        lines(l).ydata = s(l) * lines(l).ydata;
+        lines(l).ydata = bsxfun(@times, s(l,:), lines(l).ydata);
     end
