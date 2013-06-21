395b0f4f37d05329928e6fd4bacb6bec9d3921a4
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-06-21 10:59:21 -0400

    plot.state_dwell: a few more label-related edits

--- src/+ebfret/+plot/state_dwell.m
+++ src/+ebfret/+plot/state_dwell.m
@@ -26,16 +26,36 @@ function lines = state_dwell(alpha, varargin)
     %   <property> : {<values>} 
     %       Any other line properties (see doc line_props). Entries 
     %       can contain either a single or  
+    ip = inputParser();
+    ip.StructExpand = true;
+    ip.KeepUnmatched = true;
+    ip.addParamValue('labels', {}, @iscell);       
+    ip.parse(varargin{:});
+    args = ip.Results;
+
     K = size(alpha, 1);
-    lines = struct(varargin{:});
+
+    % initialize labels if necessary
+    if isempty(args.labels)
+        args.labels = arrayfun(@(k) sprintf('state %d', k), ...
+                        1:K, ...
+                        'uniformoutput', false);
+    end
+
+    % get line properties from unmatched params
+    props = cat(2, fieldnames(ip.Unmatched), struct2cell(ip.Unmatched))';
+    lines = struct(props{:}, 'displayname', args.labels);
     if isscalar(lines)
         lines(1:K) = lines;
     end
+
     % get beta distribution parameters for state occupancy
     for k = 1:K
         a(k,:) = alpha(k,k,:);
     end
     b = reshape(sum(alpha,2), [K size(a,2)]) - a;
+
+    % determine x axis range if not specified
     if ~isfield(lines, 'xdata')
         for k = 1:K
             % % rho = exp(-1/tau) = a / (a+b)
@@ -52,11 +72,12 @@ function lines = state_dwell(alpha, varargin)
             E_tau = -1 ./ log(E_rho);
             % V_rho = a(k) * b(k) / ((a(k) + b(k)).^2 * (a(k) + b(k) + 1))
             % V_tau = E_tau^2 / E_rho * V_rho
-            lines(k).xdata = linspace(1, 6 * mean(E_tau), 101)';
-                % linspace(max(0, E_tau - 4 * V_tau^0.5), ...
-                %          E_tau + 4 * V_tau^0.5, 100);
+            lines(k).xdata = ...
+                exp(linspace(log(0.01 * median(E_tau,2)), log(100 * median(E_tau,2)), 101))';
         end
     end
+    
+    % generate y values for plots
     for k = 1:K
         lines(k).ydata = ...
             bsxfun(@times, ...
