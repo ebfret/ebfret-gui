def120afd3b47e1693da029d9da40e25c872f3b7
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-06-22 18:21:03 -0400

    added code for command-line batch-mode analysis

---
+++ src/+ebfret/+batch/ebayes.m
@@ -0,0 +1,131 @@
+function [analysis, options] = ebayes(series, options, clip)
+    % analysis = ebayes(series, options, clip)
+    %
+    % Run empirical bayes analysis job(s).
+
+    % clip and crop time series
+    if nargin < 3
+        clip.min = -inf;
+        clip.max = inf;
+    end
+    for n = 1:length(series)
+        s = series(n);
+        if ~s.exclude
+            x{n} = s.signal(s.crop.min:s.crop.max);
+            % clip to specified range
+            if clip.min < inf
+                tclip = find(x{n} < clip.min);
+                x{n}(tclip) = clip.min;
+            end
+            if clip.max < inf
+                tclip = find(x{n} > clip.max);
+                x{n}(tclip) = clip.max;
+            end
+        else
+            x{n} = [];
+        end
+    end
+
+    for o = 1:length(options(:))
+        opt = options(o);
+        % initialize prior
+        if ~isfield(opt, 'prior') || isempty(opt.prior.mu)
+            if ~isfield(opt, 'init')
+                % guess prior
+                try 
+                    K = opt.dim.states;
+                catch 
+                    K = b;
+                end
+                analysis(o).prior = ebfret.analysis.hmm.guess_prior(x, K);
+            else
+                % initialize prior from specified settings
+                analysis(o).prior = ebfret.analysis.hmm.init_prior(...
+                                        opt.init.theta, opt.init.counts);
+            end
+            analysis(o).dim.states = length(analysis(o).prior.mu);
+            % store resulting prior in opt
+            opt.prior = analysis(o).prior;
+        else 
+            % use specified initial guess for prior
+            analysis(o).prior = opt.prior;
+        end
+        % store number of states
+        K = length(analysis(o).prior.mu);
+        analysis(o).dim.states = K; 
+        opt.dim.states = K; 
+
+        % initialize convergence options
+        if ~isfield(opt, 'eb')
+            opt.eb = struct();
+        end
+        if ~isfield(opt.eb, 'max_iter')
+            opt.eb.max_iter = 100;
+        end
+        if ~isfield(opt.eb, 'threshold')
+            opt.eb.threshold = 1e-4;
+        end
+        if ~isfield(opt, 'vb')
+            opt.vb = struct();
+        end
+        if ~isfield(opt.vb, 'max_iter')
+            opt.vb.max_iter = opt.eb.max_iter;
+        end
+        if ~isfield(opt.vb, 'threshold')
+            opt.vb.threshold = 0.1 * opt.eb.threshold;
+        end
+        if ~isfield(opt.vb, 'restarts')
+            opt.vb.restarts = 2;
+        end
+
+        % analysis
+        it = 1;
+        analysis(o).posterior = analysis(o).prior([]);
+        while true
+            % run variational bayes on each time series
+            [posterior, expect, lowerbound, viterbi] = ...
+                ebfret.batch.vbayes(x, analysis(o).prior, ...
+                    'posterior', analysis(o).posterior, opt.vb);
+            ns = find(~[series.exclude]);
+            analysis(o).posterior(ns) = posterior;
+            analysis(o).expect(ns) = expect;
+            analysis(o).lowerbound(ns) = lowerbound;
+            analysis(o).viterbi(ns) = viterbi;
+
+            % calculate and print out sum lower bound
+            L(it) = sum(analysis(o).lowerbound);
+            if it == 1
+                fprintf('it %02d   L %.5e\n', it, L(it))
+            else
+                fprintf('it %02d   L %.5e    dL %.2e\n', it, L(it), (L(it)-L(it-1)) / abs(L(it)));
+            end
+
+            % check if max iterations reached
+            if (it > opt.eb.max_iter)
+               break
+            end
+            
+            % check convergence
+            if (it > 1) ...
+               && ((L(it) - L(it-1)) < opt.eb.threshold * abs(L(it)))  ...
+                break
+            end
+
+            % run iterative empirical bayes update
+            % (assuming constant posterior statistics)
+            analysis(o).prior = ebfret.analysis.hmm.h_step(...
+                                    analysis(o).posterior, ...
+                                    analysis(o).prior, ...
+                                    'expect', analysis(o).expect);
+
+            % increment iteration counter
+            it = it + 1;
+        end
+        opt.L = L;
+
+        % store 
+        opts(o) = opt;
+    end
+    analysis = reshape(analysis, size(options));
+    options = reshape(opts, size(options));
+end
---
+++ src/+ebfret/+batch/vbayes.m
@@ -0,0 +1,96 @@
+function [posterior, expect, lowerbound, viterbi, restart] = vbayes(x, prior, varargin)
+    % analysis = ebayes(series, varargin)
+    %
+    % Run empirical bayes analysis batch job
+    ip = inputParser();
+    ip.StructExpand = true;
+    ip.addParamValue('posterior', []);       
+    ip.addParamValue('restarts', 0, @isscalar);       
+    ip.addParamValue('threshold', 1e-6, @isscalar);       
+    ip.addParamValue('max_iter', 100, @isscalar);       
+    ip.addParamValue('soft_kmeans', false, @isscalar);       
+    ip.parse(varargin{:});
+    args = ip.Results;
+
+    % output variables for parfor loop
+    posterior = {};
+    expect = {};
+    viterbi = {};
+    lowerbound = {};
+    restart = {};
+
+    % parallel loop over traces
+    u = prior;
+    parfor n = 1:length(x)
+        if ~isempty(x{n})
+            % construct initial guesses for posterior parameters
+            w0 = u([]);
+            try 
+                % always add a restart based on last result if it exists
+                if ~isempty(args.posterior(n).mu)
+                    w0(end+1) = args.posterior(n);
+                end
+            catch
+                % if posterior not supplied this fails (but no biggie)
+            end
+            if args.restarts > 0
+                % first restart is uninformative
+                w0(end+1) = ebfret.analysis.hmm.init_posterior(x{n}, u);
+            end
+            for r = 2:args.restarts
+                % next restarts are seeded with random draws from prior
+                w0(end+1) = ebfret.analysis.hmm.init_posterior(...
+                                x{n}, u, ...
+                                'draw_params', true, ...
+                                'soft_kmeans', args.soft_kmeans);
+            end
+            
+            % run variational bayes for each restart
+            vb = struct();
+            for r = 1:length(w0)
+                [vb(r).w vb(r).L vb(r).E] = ...
+                    ebfret.analysis.hmm.vbayes(x{n}, w0(r), u);
+            end
+            
+            % determine best result
+            L_max = vb(1).L(end);
+            r_max = 1;
+            for r = 2:length(vb)
+                if (vb(r).L(end) - L_max) > 1e-2 * args.threshold * abs(L_max) ...
+                    || isnan(L_max)
+                    r_max = r;
+                    L_max = vb(r).L(end);
+                end
+            end
+
+            % keep best result
+            lowerbound{n} = vb(r_max).L(end);
+            posterior{n} = vb(r_max).w;
+            restart{n} = r_max + args.restarts - length(w0);
+
+            % remap expectated statistics
+            expect{n}.z = sum(vb(r_max).E.gamma(2:end,:),1)';
+            expect{n}.z1 = vb(r_max).E.gamma(1,:)';
+            expect{n}.zz = squeeze(sum(vb(r_max).E.xi, 1));
+            expect{n}.x = vb(r_max).E.xmean(:);
+            expect{n}.xx = vb(r_max).E.xvar + vb(r_max).E.xmean.^2;
+
+            % calculate viterbi path
+            [viterbi{n}.state, viterbi{n}.mean] = ...
+                ebfret.analysis.hmm.viterbi_vb(vb(r_max).w, x{n});
+        else
+            posterior{n} = [];
+            expect{n} = [];
+            viterbi{n} = [];
+            lowerbound{n} = [];
+            restart{n} = [];
+        end
+    end
+
+    % reformat output to struct array
+    posterior = [posterior{:}];
+    expect = [expect{:}];
+    viterbi = [viterbi{:}];
+    lowerbound = [lowerbound{:}];
+    restart = [restart{:}];
+end
