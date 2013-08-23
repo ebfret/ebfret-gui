6a40393605f700264270fc258dccffd44e57e1b3
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-08-23 13:55:33 -0400

    ui.IndexControl: adjust slider minor step size to increments of 1

--- src/+ebfret/+ui/IndexControl.m
+++ src/+ebfret/+ui/IndexControl.m
@@ -68,24 +68,24 @@ classdef IndexControl < hgsetget
                             'min', args.min-eps, ...
                             'max', args.max+eps, ...
                             'value', args.value, ...
-                            'sliderstep', [1 1], ...
+                            'sliderstep', [0 0.1], ...
                             'callback', @(source, event) self.control_callback(source, event), ...
                             'units', 'normalized', ...
-                            'position', [0.1+hp, vp, 0.7-hp, sh]);
-            self.handles.prevButton ...
-                = uicontrol('parent', handles.panel, ...
-                            'style', 'push', ...
-                            'string', 'Prev', ...
-                            'callback', @(source, event) self.control_callback(source, event), ...
-                            'units', 'normalized', ...
-                            'position', [0.8+hp, vp, 0.1-hp, 1-2*vp]);
-            self.handles.nextButton ...
-                = uicontrol('parent', handles.panel, ...
-                            'style', 'push', ...
-                            'string', 'Next', ...
-                            'callback', @(source, event) self.control_callback(source, event), ...
-                            'units', 'normalized', ...
-                            'position', [0.9+hp, vp, 0.1-2*hp, 1-2*vp]);
+                            'position', [0.1+hp, vp, 0.9-2*hp, sh]);
+            % self.handles.prevButton ...
+            %     = uicontrol('parent', handles.panel, ...
+            %                 'style', 'push', ...
+            %                 'string', 'Prev', ...
+            %                 'callback', @(source, event) self.control_callback(source, event), ...
+            %                 'units', 'normalized', ...
+            %                 'position', [0.8+hp, vp, 0.1-hp, 1-2*vp]);
+            % self.handles.nextButton ...
+            %     = uicontrol('parent', handles.panel, ...
+            %                 'style', 'push', ...
+            %                 'string', 'Next', ...
+            %                 'callback', @(source, event) self.control_callback(source, event), ...
+            %                 'units', 'normalized', ...
+            %                 'position', [0.9+hp, vp, 0.1-2*hp, 1-2*vp]);
             self.set_prop('min', args.min, 'max', args.max, 'value', args.value, 'callback', args.callback);
         end
         % callback wrapper
@@ -96,10 +96,10 @@ classdef IndexControl < hgsetget
                     value = round(str2num(get(self.handles.indexEdit, 'string')));
                 case self.handles.slider
                     value = round(get(self.handles.slider, 'value'));
-                case self.handles.prevButton
-                    value = self.value - 1;
-                case self.handles.nextButton
-                    value = self.value + 1;
+                % case self.handles.prevButton
+                %     value = self.value - 1;
+                % case self.handles.nextButton
+                %     value = self.value + 1;
             end
             if value ~= self.value
                 % update widget value
@@ -145,11 +145,17 @@ classdef IndexControl < hgsetget
             if isfield(properties, 'min')
                 self.min  = round(properties.min);
                 set(self.handles.slider, 'min', self.min-eps);
+                if self.max > self.min
+                    set(self.handles.slider, 'sliderstep', [1./(self.max-self.min) 0.1])
+                end
                 self.set_prop('value', max(self.value, self.min));
             end
             if isfield(properties, 'max')
                 self.max  = round(properties.max);
                 set(self.handles.slider, 'max', self.max+eps);
+                if self.max > self.min
+                    set(self.handles.slider, 'sliderstep', [1./(self.max-self.min) 0.1])
+                end
                 self.set_prop('value', min(self.value, self.max));
             end
             if isfield(properties, 'value')
@@ -161,23 +167,23 @@ classdef IndexControl < hgsetget
             if self.max == self.min
                 set(self.handles.indexEdit, 'enable', 'off'); 
                 set(self.handles.slider, 'enable', 'off'); 
-                set(self.handles.prevButton, 'enable', 'off'); 
-                set(self.handles.nextButton, 'enable', 'off'); 
+                % set(self.handles.prevButton, 'enable', 'off'); 
+                % set(self.handles.nextButton, 'enable', 'off'); 
             else
                 set(self.handles.indexEdit, 'enable', 'on'); 
                 set(self.handles.slider, 'enable', 'on'); 
-                if (self.value == self.max) | isnan(self.value)
-                    set(self.handles.nextButton, 'enable', 'off');
-                end
-                if (self.value < self.max)
-                    set(self.handles.nextButton, 'enable', 'on');
-                end
-                if (self.value == self.min) | isnan(self.value)
-                    set(self.handles.prevButton, 'enable', 'off');
-                end
-                if (self.value > self.min)
-                    set(self.handles.prevButton, 'enable', 'on');
-                end
+                % if (self.value == self.max) | isnan(self.value)
+                %     set(self.handles.nextButton, 'enable', 'off');
+                % end
+                % if (self.value < self.max)
+                %     set(self.handles.nextButton, 'enable', 'on');
+                % end
+                % if (self.value == self.min) | isnan(self.value)
+                %     set(self.handles.prevButton, 'enable', 'off');
+                % end
+                % if (self.value > self.min)
+                %     set(self.handles.prevButton, 'enable', 'on');
+                % end
             end
         end
     end
