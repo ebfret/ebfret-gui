244b70c2bef834f20b975efaa64841a5874b952d
Jan-Willem van de Meent <janwillem.vandemeent@columbia.edu>
2013-12-01 15:09:12 -0500

    ui.MainWindow.update_priors: reset posterior after updating prior

--- src/+ebfret/+ui/@MainWindow/update_priors.m
+++ src/+ebfret/+ui/@MainWindow/update_priors.m
@@ -9,8 +9,11 @@ function update_priors(self, choice)
             num_states = ...
                 self.controls.min_states:self.controls.max_states; 
             for k = num_states(num_states>0)
+                % update prior
                 self.analysis(k).prior = ...
                     ebfret.analysis.hmm.guess_prior(self.get_signal(), k);
+                % reset posteriors
+                self.reset_posterior(k);
             end
         case 'manual'
             self.init_priors()
